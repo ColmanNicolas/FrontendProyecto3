308dd169f4534712c3c70915a25eb2f0375a069f
ColmanNicolas <colman.nicolas.17@hotmail.com>
2024-03-21 19:08:41 -0300

    Estructura inicial para la aplicacion React

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+
+Home
+
+  return (
+    <>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Navigate to={"home"} />} />
+      <Route path='/home' element={<Home/>} />
+      <Route/>
+      <Route/>
+    </Routes>
+  </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+const Home = ()=>{
+    return(
+        <h1>SOY EL HOME</h1>
+    )
+};
+export default Home;
